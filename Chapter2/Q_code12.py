173a6b7733521569256a4559238c6e983ece1fe5
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-16 09:12:53 +0530

    typecasting,inputfunction

---
+++ Chapter2/Q_code12.py
@@ -0,0 +1,6 @@
+# addition of two numbers using explicit casting concept
+# int() function is used to covert the string values of a and b into integer
+a="32"
+b="45"
+print(int(a)+int(b))
+
--- Chapter2/Q_code5.py
+++ Chapter2/Q_code5.py
@@ -1,5 +1,6 @@
 # Write a python program to add two numbers
 # here int() is used before input() to convert user's input (string) into an integer
+# if didn't use int function then python will concatenate assuming a and b as string 
 a =int(input("Enter the num1 : "))
 b =int(input("Enter the num2 : "))
 print("The addition is :",(a+b))
