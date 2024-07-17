05505d1ad656ae190c250f1f979a7f9193569d6c
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-17 22:36:05 +0530

    Access string element

---
+++ Chapter2/Q_code15.py
@@ -0,0 +1,12 @@
+# print the element from the given index of string
+a='Sanskruti'
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
+print(a[7])
+print(a[8])
+print(a[9]) #this will throw index error as length of string is 8
