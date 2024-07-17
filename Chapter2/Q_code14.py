9740b2e8949ca50898058565967b3812ae56bc21
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-17 22:31:44 +0530

    string

---
+++ Chapter2/Q_code14.py
@@ -0,0 +1,9 @@
+# string concepts
+# 1. print the value in string variable
+name="Sakshi"
+print("My name is",name) # it prints the string followed by the value in the string variable 
+
+# 2. print the string which includes double quotations
+print('He said,"she is my sister"') # whole string is enclosed in single quotations to avoid syntax errors
+# to print the above using double quotations and avoid syntax error we use backslash
+print("He said,\"she is my sister\"")
