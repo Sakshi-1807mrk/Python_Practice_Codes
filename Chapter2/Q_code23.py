0f732570ab3fc06a13d8376961792595f7270e60
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-20 00:35:31 +0530

    elif statement

---
+++ Chapter2/Q_code23.py
@@ -0,0 +1,10 @@
+# if-elif-else
+
+num=int(input("Enter the number:"))
+if(num>0):
+    print("Number is positive")
+elif(num==0):
+    print("Number is zero")#executes when if condition is false
+else:
+    print("Number is negative")    
+
