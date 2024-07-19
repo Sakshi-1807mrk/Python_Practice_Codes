27d67b76480f6180c17a7336ed0d6ba266669412
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-20 00:34:29 +0530

    if-else statement

---
+++ Chapter2/Q_code22.py
@@ -0,0 +1,8 @@
+#  if else statement
+
+budget=100
+mango_price=80
+if(mango_price<=budget):
+    print("You can purchase Mango")#executes when if condition true
+else:
+    print("You can purchase Banans")#executes when if conditon is false
