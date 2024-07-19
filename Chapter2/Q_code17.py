1d97c2cbf7a660b3d1cce072430d813cf89052f8
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-19 19:12:03 +0530

    string slicing

---
+++ Chapter2/Q_code17.py
@@ -0,0 +1,16 @@
+# string slicing
+
+fruit='banana'
+print(fruit[:6]) #slicing from start
+print(fruit[3:]) #slicing till end
+print(fruit[2:6]) #slicing in between
+print(fruit[-6:]) #slicing using negative index
+print("-------------")
+# negative slicing
+a='Sakshi'
+print(a[-4:-2])
+
+# slicing with skip values
+
+a='Mango'
+print(a[2:3:5])
