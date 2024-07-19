19f874f3d01862489dab5f3a388b9e10410f3dd0
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-19 19:13:24 +0530

    looping through string

---
+++ Chapter2/Q_code18.py
@@ -0,0 +1,10 @@
+# loop through a string
+
+string='With God everything is possible'
+for i in string:
+ print(i)
+print("-------------------")
+string='With God everything is possible'
+str=string.split(' ')
+for i in str:
+ print(i)
