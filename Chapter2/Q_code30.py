ee479678e4b4ef01c0a42641f9d73bd96ed2f6e4
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-20 08:39:12 +0530

    Exercise Solution

---
+++ Chapter2/Q_code30.py
@@ -0,0 +1,9 @@
+# write a program to find out whether a given blog is talking about Elon Musk or not
+blog='''Elon Musk continued gripes with California, where more than one in five new cars sold are electric, 
+is losing him customers to Rivian.Industry data on Thursday revealed demand for new Teslas in the Golden State 
+plummeted 24% to 52,211 vehicles in Q2, based on registrations recorded post-sale when a vehicle’s title is processed. '''
+
+if("Elon Musk"in blog):
+    print("The blog talks about Elon Musk")
+else:
+    print("This blog doesn't talk about Elon Musk")
