6026412a4a3da7daa4d8fea85a425fa41a31d264
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-21 17:36:05 +0530

    solved problems on loop

---
+++ Chapter2/Q_code35.py
@@ -0,0 +1,6 @@
+# print the contents of list using while loop
+list = ["good","brilliant","excellent","outstanding","welldone"]
+i=0
+while(i<len(list)): # len() to get the length of the list
+    print(f"I am {list[i]}") # list[i] will print the value at index i
+    i+=1
