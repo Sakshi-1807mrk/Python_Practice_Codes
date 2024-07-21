c71d290c3edf0d92ba23357f58d4ae2febd8b04e
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-21 17:38:43 +0530

    pass statement

---
+++ Chapter2/Q_code38.py
@@ -0,0 +1,7 @@
+# pass statement
+# program to put a pass statement in your for loop to avoid an error.
+lst=[9, 10, 100, 999]
+for i in lst:
+    pass 
+# pass statement instructs to do nothing to the inerpreter without getting error
+# without pass the program may throw error
