a9fdc1d5bf87288e26b52b089a3a7ac6d9e87279
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-21 17:31:55 +0530

    for loop statement

---
+++ Chapter2/Q_code33.py
@@ -0,0 +1,15 @@
+# for loop
+
+fruits=["apple","mango","orange","cherry"]
+# The loop will iterate over the fruits list and assign each value to the fruit variable. 
+for fruit in fruits :
+    print(fruit)#executes the print statement for each one.
+print("-------------------------------------")
+
+# using range() function for iterating over a sequence of numbers
+for x in range(6): # range() generates value from 0 to n-1 here 5
+    print(x)
+print("-------------------------------------")
+
+for y in range(2,10):
+    print(y) # generates value from n to n-1
