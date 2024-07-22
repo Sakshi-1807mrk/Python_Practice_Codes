eefab5713630256320f2b1d14fdc6aba9ee3c492
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-07-22 13:15:47 +0530

    star pattern problem using loop

---
+++ Chapter2/Q_code48.py
@@ -0,0 +1,11 @@
+# program to print the following star pattern 
+#  * * * * *
+#  * *
+#  * * 
+n=3
+n1=1
+for i in range(n1,n+1):
+    if(i==1):
+     print("* "*5)
+    else:
+       print("* "*2)
