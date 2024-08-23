e5b7de808554b9d0afd903ff6796e9d49744f20d
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-08-23 21:01:01 +0530

    Math Module

---
+++ Chapter3/Q_code72.py
@@ -0,0 +1,23 @@
+# math module in python
+import math
+
+# constants 
+# Euler's Number math.e
+print(math.e)
+# pi 
+print(math.pi)
+
+# functions 
+# floor() function retrurns integer not greater than number
+a=34.678
+print(math.floor(a))
+# sqrt() function returns square root of number
+b=81
+print(math.sqrt(b))
+# pow(x,y) function returns x raise to power y
+x=2
+y=3
+print(math.pow(x,y))
+# fabs() function returns the absolute number
+c=-12
+print(math.fabs(c))
