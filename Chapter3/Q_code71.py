843b4ea160975768b99f4f14746a905b3362ee18
Sakshi-1807mrk <sakshikhode21@gmail.com>
2024-08-23 20:12:03 +0530

    Number Conversion System

---
+++ Chapter3/Q_code71.py
@@ -0,0 +1,29 @@
+# Conversion from Decimal to Binary,Octal,Hexadecimal
+
+a=int(input("Enter Decimal number: " ),9)
+print(bin(a))
+print(oct(a))
+print(hex(a))
+
+# Conversion from Binary  to Decimal,Octal,Hexadecimal
+
+b=int(input("Enter binary number :  "),2)
+print(b)
+print(oct(b))
+print(hex(b))
+
+# Conversion from Octal to Decimal,Binary,Hexadecimal
+
+c=int(input("Enter Octal numbers :"),8)
+print(c)
+print(bin(c))
+print(hex(c))
+
+# Conversion from Octal to Decimal,Binary,Hexadecimal
+
+d=int(input("Enter Hexadecimal number :"),16)
+print(d)
+print(bin(c))
+print(oct(b))
+
+
